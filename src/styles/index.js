626e55be38a1bd49d1f73884f71d6fd1f22b3109
duon0060 <duon0060@algonquinlive.com>
2021-03-04 14:42:26 -0500

    use style module

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,6 @@
-import * as colors from './colors'
-import * as spacing from './spacing'
-import * as themes from './themes'
-import * as typography from './typography'
+import * as colors from './colors';
+import * as spacing from './spacing';
+import * as themes from './themes';
+import * as typography from './typography';
 
-export {colors, spacing, themes, typography}
+export {colors, spacing, themes, typography};
