fa4b7871bf8c792ab8b367e0735116e029e1dfba
duon0060 <duon0060@algonquinlive.com>
2021-03-09 20:45:10 -0500

    make news screen

---
+++ src/screens/news/NewsScreen.js
@@ -0,0 +1,17 @@
+//NewsScreen module
+
+// import packages
+import React from 'react';
+
+import {Text} from 'react-native';
+
+//function return
+function NewsScreen(props) {
+  return (
+    <>
+      <Text>News Screen</Text>
+    </>
+  );
+}
+
+export default NewsScreen;
