9b516260776bc795fd548bea9fc9d61657aacf6c
duon0060 <duon0060@algonquinlive.com>
2021-03-09 20:45:19 -0500

    make screen stack

--- src/screens/news/NewsStack.js
+++ src/screens/news/NewsStack.js
@@ -4,12 +4,17 @@
 import React from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
+import NewsScreen from './NewsScreen';
 
 const News = createStackNavigator();
 
 //function return
 function NewsStack(props) {
-  return <News.Navigator></News.Navigator>;
+  return (
+    <News.Navigator>
+      <News.Screen name="News" component={NewsScreen} />
+    </News.Navigator>
+  );
 }
 
 export default NewsStack;
