7af186c2aeace357cd4705067eff441c5585c832
duon0060 <duon0060@algonquinlive.com>
2021-03-09 20:44:31 -0500

    make home screen

---
+++ src/screens/home/HomeScreen.js
@@ -0,0 +1,20 @@
+//HomeScreen module
+
+// import packages
+import React from 'react';
+
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import FocusedStatusBar from '../../components/FocusedStatusBar';
+
+//function return
+function HomeScreen(props) {
+  return (
+    <SafeAreaView style={{flex: 1}} edges={['right', 'bottom', 'left']}>
+      <FocusedStatusBar barStyle="light-content" />
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+export default HomeScreen;
