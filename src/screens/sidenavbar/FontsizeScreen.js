a3bbc07e2c9d3e38924d027da635b32802115263
duon0060 <duon0060@algonquinlive.com>
2021-03-10 16:43:14 -0500

    Make Fontsize Screen

---
+++ src/screens/sidenavbar/FontsizeScreen.js
@@ -0,0 +1,20 @@
+//FontsizeScreen.js
+
+// import packages
+import React from 'react';
+
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import FocusedStatusBar from '../components/FocusedStatusBar';
+
+//function return
+function FontsizeScreen(props) {
+  return (
+    <SafeAreaView style={{flex: 1}} edges={['right', 'bottom', 'left']}>
+      <FocusedStatusBar barStyle="light-content" />
+      <Text>FontsizeScreen</Text>
+    </SafeAreaView>
+  );
+}
+
+export default FontsizeScreen;
