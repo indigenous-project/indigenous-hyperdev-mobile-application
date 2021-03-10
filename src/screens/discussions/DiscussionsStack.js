3a2b328d4708053682dd28fcec16e7d1635e2a40
duon0060 <duon0060@algonquinlive.com>
2021-03-09 20:44:22 -0500

    make discussion stack

--- src/screens/discussions/DiscussionsStack.js
+++ src/screens/discussions/DiscussionsStack.js
@@ -4,11 +4,16 @@
 import React from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
+import DiscussionScreen from './DiscussionScreen';
 
 const Discussion = createStackNavigator();
 //function return
 function DiscussionsStack(props) {
-  return <Discussion.Navigator></Discussion.Navigator>;
+  return (
+    <Discussion.Navigator>
+      <Discussion.Screen name="DiscussionScreen" component={DiscussionScreen} />
+    </Discussion.Navigator>
+  );
 }
 
 export default DiscussionsStack;
