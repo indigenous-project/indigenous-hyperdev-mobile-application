f04a647193601f3a4aba345bbbec1691b80d0659
duon0060 <duon0060@algonquinlive.com>
2021-03-09 20:44:44 -0500

    make home stack

--- src/screens/home/HomeStack.js
+++ src/screens/home/HomeStack.js
@@ -4,11 +4,23 @@
 import React from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import {themes} from '../../styles';
 
 const Home = createStackNavigator();
+const theme = themes.light;
 //function return
 function HomeStack(props) {
-  return <Home.Navigator></Home.Navigator>;
+  return (
+    <Home.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: theme.primaryColor},
+        headerTintColor: theme.inverseTextColor,
+      }}
+      r>
+      <Home.Screen name="Indigenous Brigde" component={HomeScreen} />
+    </Home.Navigator>
+  );
 }
 
 export default HomeStack;
