571ab6e893eed07f0a18cc8176e4cd3d2b34928a
duon0060 <duon0060@algonquinlive.com>
2021-03-11 17:09:54 -0500

    add hambuger menu

--- src/screens/home/HomeStack.js
+++ src/screens/home/HomeStack.js
@@ -6,19 +6,20 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import {themes} from '../../styles';
+import HambugerMenuHeader from '../../components/HambugerMenuHeader';
 
 const Home = createStackNavigator();
 const theme = themes.light;
 //function return
-function HomeStack(props) {
+function HomeStack({navigation}) {
   return (
     <Home.Navigator
       screenOptions={{
-        headerStyle: {backgroundColor: theme.primaryColor},
-        headerTintColor: theme.inverseTextColor,
-      }}
-      r>
-      <Home.Screen name="Indigenous Brigde" component={HomeScreen} />
+        headerStyle: {backgroundColor: theme.primaryColor}, //header background
+        headerTintColor: theme.inverseTextColor, // text color
+        headerLeft: () => <HambugerMenuHeader navigationProps={navigation} />, // implement hambuger menu on the left of the header
+      }}>
+      <Home.Screen name="Indigenous Bridge" component={HomeScreen} />
     </Home.Navigator>
   );
 }
