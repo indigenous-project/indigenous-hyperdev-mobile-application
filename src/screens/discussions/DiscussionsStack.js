bf0a9ba435fe1ccddf15bddac7909030d5b6c567
duon0060 <duon0060@algonquinlive.com>
2021-03-11 17:09:19 -0500

    add hambuger menu

--- src/screens/discussions/DiscussionsStack.js
+++ src/screens/discussions/DiscussionsStack.js
@@ -5,12 +5,20 @@ import React from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import DiscussionScreen from './DiscussionScreen';
+import {themes} from '../../styles';
+import HambugerMenuHeader from '../../components/HambugerMenuHeader';
 
+const theme = themes.light;
 const Discussion = createStackNavigator();
 //function return
 function DiscussionsStack(props) {
   return (
-    <Discussion.Navigator>
+    <Discussion.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: theme.primaryColor}, //header background
+        headerTintColor: theme.inverseTextColor, // text color
+        headerLeft: () => <HambugerMenuHeader />, // implement hambuger menu on the left of the header
+      }}>
       <Discussion.Screen name="DiscussionScreen" component={DiscussionScreen} />
     </Discussion.Navigator>
   );
