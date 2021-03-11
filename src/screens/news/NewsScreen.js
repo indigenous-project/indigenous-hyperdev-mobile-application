7e3b43caf9641a4d108858a5019eff8b8f826794
duon0060 <duon0060@algonquinlive.com>
2021-03-11 17:10:26 -0500

    add safe area

--- src/screens/news/NewsScreen.js
+++ src/screens/news/NewsScreen.js
@@ -4,13 +4,15 @@
 import React from 'react';
 
 import {Text} from 'react-native';
-
+import {SafeAreaView} from 'react-native-safe-area-context';
+import FocusedStatusBar from '../../components/FocusedStatusBar';
 //function return
 function NewsScreen(props) {
   return (
-    <>
+    <SafeAreaView style={{flex: 1}} edges={['right', 'bottom', 'left']}>
+      <FocusedStatusBar barStyle="light-content" />
       <Text>News Screen</Text>
-    </>
+    </SafeAreaView>
   );
 }
 
