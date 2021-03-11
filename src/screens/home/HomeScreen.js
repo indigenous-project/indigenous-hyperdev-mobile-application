745561c79b29e0c28770d03e3d442b51f2c4771e
duon0060 <duon0060@algonquinlive.com>
2021-03-11 17:09:42 -0500

    testing get a list of categories api

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,18 +1,29 @@
 //HomeScreen module
 
 // import packages
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import {Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {categoriesGetList} from '../../api/categories/categories.api';
 import FocusedStatusBar from '../../components/FocusedStatusBar';
 
 //function return
 function HomeScreen(props) {
+  const [categories, setCategories] = useState(null);
+
+  useEffect(() => {
+    categoriesGetList(
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDQ3YzY4NDkwZTc4MjAwMDdmZjg2ZTEiLCJpYXQiOjE2MTUzMTY2Mjh9.AOe0KoE5MRZN1xw2hMEI3Tq28QPeASkc8BAugpHEChc',
+    )
+      .then(setCategories)
+      .catch(console.log);
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}} edges={['right', 'bottom', 'left']}>
       <FocusedStatusBar barStyle="light-content" />
-      <Text>Home Screen</Text>
+      <Text>{JSON.stringify(categories)}</Text>
     </SafeAreaView>
   );
 }
