80411bc95fcb446c9a7a4ec620dd3a451657311a
duon0060 <duon0060@algonquinlive.com>
2021-03-11 17:12:46 -0500

    add stack navigator

--- src/screens/services/ServicesStack.js
+++ src/screens/services/ServicesStack.js
@@ -5,13 +5,21 @@ import React from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import ServiceScreen from './ServiceScreen';
+import {themes} from '../../styles';
+import HambugerMenuHeader from '../../components/HambugerMenuHeader';
 
+const theme = themes.light;
 const Services = createStackNavigator();
 
 //function return
 function ServicesStack(props) {
   return (
-    <Services.Navigator>
+    <Services.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: theme.primaryColor}, //header background
+        headerTintColor: theme.inverseTextColor, // text color
+        headerLeft: () => <HambugerMenuHeader />, // implement hambuger menu on the left of the header
+      }}>
       <Services.Screen name="Services" component={ServiceScreen} />
     </Services.Navigator>
   );
