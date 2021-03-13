61a54c36ea6775180adc782b0bf1821ba9aeb774
Minal Patel <pate0773@algonquinlive.com>
2021-03-12 23:27:07 -0500

    Container to display New Survey by using Custom Survey card

--- src/screens/news/NewsScreen.js
+++ src/screens/news/NewsScreen.js
@@ -4,6 +4,7 @@
 import React from 'react';
 
 import { View, ScrollView, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import { color } from 'react-native-reanimated';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FocusedStatusBar from '../../components/FocusedStatusBar';
 import { colors, themes } from '../../styles';
@@ -24,6 +25,21 @@ function NewsScreen(props) {
     );
   }
 
+  //card to display New Survey Available
+  const SurveyCard = (props) => {
+    return (
+      <View style={styles.newSurvey}>
+        <Image style={styles.image} source={require('../../testImages/demoPic.png')} />
+        <View style={styles.surveyTitle}>
+          <Text style={styles.surveyText}>{props.surveyText}</Text>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>Start Survey</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }} edges={['right', 'bottom', 'left']}>
       <FocusedStatusBar barStyle="light-content" />
@@ -34,7 +50,7 @@ function NewsScreen(props) {
         {/* <Text>{JSON.stringify(categories)}</Text> */}
 
         <View style={styles.container}>
-          <View style={styles.jobHeading}>
+          <View style={styles.containerHeading}>
             <Text style={styles.heading}>Job News</Text>
             <Text>See All</Text>
           </View>
@@ -50,6 +66,14 @@ function NewsScreen(props) {
               ></JobCard>
             </ScrollView>
           </View>
+        </View>
+
+        <View style={styles.container}>
+          <View style={styles.containerHeading}>
+            <Text style={styles.heading}>New Survey</Text>
+            <Text>See All</Text>
+          </View>
+          <SurveyCard surveyText="Aboriginal Peoples Survey Concepts and Methods"></SurveyCard>
         </View>
 
       </ScrollView>
@@ -74,9 +98,7 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     fontSize: 16
   },
-
-  //job container styles
-  jobHeading: {
+  containerHeading: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: "95%",
@@ -85,21 +107,22 @@ const styles = StyleSheet.create({
   jobNews: {
     flexDirection: "row",
   },
+
   //job card styles
   JobCard: {
     margin: 10,
     padding: 10,
     borderRadius: 10,
     shadowOffset: { width: 3, height: 6 },
-    shadowColor: 'black',
-    backgroundColor: 'white',
+    shadowColor: colors.gray900,
+    backgroundColor: colors.white,
     minWidth: "60%",
     shadowOpacity: 0.2,
   },
   cardTitle: {
     fontWeight: "700",
     paddingBottom: 10,
-    color: "#6F1818",
+    color: colors.primary900,
     fontSize: 18
   },
   cardSubTitle: {
@@ -111,5 +134,50 @@ const styles = StyleSheet.create({
     fontWeight: "300",
     paddingBottom: 10,
     fontSize: 16
+  },
+
+  //survey card style
+  newSurvey: {
+    flexDirection: 'row',
+    borderRadius: 10,
+    margin: 10,
+    maxWidth: "95%",
+    borderWidth: 0.1,
+    shadowOffset: { width: 3, height: 6 },
+    shadowColor: colors.gray900,
+    backgroundColor: colors.white,
+    shadowOpacity: 0.2,
+  },
+  image: {
+    borderRadius: 10,
+    width: 110,
+    height: 110,
+    marginVertical: 10,
+    marginLeft: 10
+  },
+  surveyTitle: {
+    display: "flex",
+    justifyContent: "center",
+    width: "70%",
+    alignItems: "center"
+  },
+  surveyText: {
+    fontSize: 16,
+    lineHeight: 25,
+    fontWeight: "500",
+    paddingBottom: 10,
+  },
+  buttonContainer: {
+    marginBottom: 10,
+    backgroundColor: colors.primary500,
+    minWidth: "50%",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
+  buttonText: {
+    fontSize: 14,
+    color: colors.white,
+    alignSelf: "center",
   },
 })
