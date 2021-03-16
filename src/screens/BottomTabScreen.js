2f248166c24540d8a5dc621414379c9e4d5fcc4a
duon0060 <duon0060@algonquinlive.com>
2021-03-16 00:41:48 -0400

    config use vector icons

--- src/screens/BottomTabScreen.js
+++ src/screens/BottomTabScreen.js
@@ -7,12 +7,21 @@ import DiscussionsStack from './discussions/DiscussionsStack';
 import NewsStack from './news/NewsStack';
 import ServicesStack from './services/ServicesStack';
 import OrganizationsStack from './organizations/OrganizationsStack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 function BottomTabScreen(props) {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen
+        name="Home"
+        component={HomeStack}
+        options={{
+          tabBarIcon: ({focused, size, color}) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
       <Tab.Screen name="Discussions" component={DiscussionsStack} />
       <Tab.Screen name="News" component={NewsStack} />
       <Tab.Screen name="Services" component={ServicesStack} />
