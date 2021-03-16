f14ee5b15c2dfd96e603fd61026b5c2c396b14aa
duon0060 <duon0060@algonquinlive.com>
2021-03-15 21:14:37 -0400

    add Stack screen Auth

--- App.js
+++ App.js
@@ -6,11 +6,13 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {createStackNavigator} from '@react-navigation/stack';
 import DrawerRoute from './src/screens/sidenavbar/DrawerRoute';
+import AuthStack from './src/screens/authentication/AuthStack';
 
 const Stack = createStackNavigator();
 const App = () => {
@@ -18,6 +20,12 @@ const App = () => {
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator>
+          <Stack.Screen
+            name="Auth"
+            component={AuthStack}
+            options={{headerShown: false, animationTypeForReplace: 'pop'}}
+          />
+
           <Stack.Screen
             name="DrawerRoute"
             component={DrawerRoute}
