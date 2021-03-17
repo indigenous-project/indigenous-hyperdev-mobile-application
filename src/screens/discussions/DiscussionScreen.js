0f5c0f61cd021c37786b54e7af9df55427984e64
duon0060 <duon0060@algonquinlive.com>
2021-03-17 17:52:24 -0400

    formatting date

--- src/screens/discussions/DiscussionScreen.js
+++ src/screens/discussions/DiscussionScreen.js
@@ -25,6 +25,15 @@ function DiscussionScreen(props) {
   const [discussions] = useDiscussion([]);
   console.log(discussions);
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = new Intl.DateTimeFormat('en', {year: 'numeric'}).format(date);
+    const month = new Intl.DateTimeFormat('en', {month: 'short'}).format(date);
+    const day = new Intl.DateTimeFormat('en', {day: '2-digit'}).format(date);
+
+    return `${month} ${day}, ${year}`;
+  };
+
   return (
     <SafeAreaView style={{flex: 1}} edges={['right', 'left']}>
       <FocusedStatusBar barStyle="light-content" />
@@ -56,7 +65,9 @@ function DiscussionScreen(props) {
               <DiscussionCard
                 key={discussion._id}
                 title={discussion.title}
-                nameAndDate={`${discussion.owner.firstName} ${discussion.owner.lastName} ${discussion.createdAt}`}
+                nameAndDate={`${discussion.owner.firstName} ${
+                  discussion.owner.lastName
+                } Posed ${formatDate(discussion.createdAt)}`}
                 description={discussion.description}
                 categories={discussion.categories}
                 replies={discussion.replies}
