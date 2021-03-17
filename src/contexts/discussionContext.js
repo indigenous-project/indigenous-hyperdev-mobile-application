bb3eea1b2d4dff98cd9c2a42fcdb468d7199f92c
duon0060 <duon0060@algonquinlive.com>
2021-03-17 11:36:47 -0400

    make discussion context get list

---
+++ src/contexts/discussionContext.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react';
+
+import {Alert} from 'react-native';
+import {discussionGetList} from '../api/discussions/discussions.api';
+import {useSecureStorage} from '../hooks/useSecureStorage';
+
+const DiscussionContext = React.createContext();
+
+function DiscussionProvider(props) {
+  const [discussions, setDiscussions] = useState([]);
+  const [token, setToken] = useSecureStorage('userToken', '');
+
+  useEffect(() => {
+    if (token)
+      discussionGetList(token)
+        .then(setDiscussions)
+        .catch((err) => Alert.alert(err.errors[0]));
+  }, [token]);
+  return <DiscussionContext.Provider value={[discussions]} {...props} />;
+}
+
+function useDiscussion() {
+  const context = React.useContext(DiscussionContext);
+  if (!context)
+    throw new Error(
+      'useDiscussion hook must be called within a DiscussionContext',
+    );
+  return context;
+}
+
+export {DiscussionProvider, useDiscussion};
