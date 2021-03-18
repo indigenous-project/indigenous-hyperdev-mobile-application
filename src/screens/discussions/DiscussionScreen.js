b3c16aed775b2522d79d621f1c56e1822092c88e
duon0060 <duon0060@algonquinlive.com>
2021-03-18 00:44:55 -0400

    filter data and refresh data behavior

--- src/screens/discussions/DiscussionScreen.js
+++ src/screens/discussions/DiscussionScreen.js
@@ -1,16 +1,18 @@
 //DiscussionScreen module
 
 // import packages
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-import {Text, StyleSheet, View, ScrollView} from 'react-native';
+import {Text, StyleSheet, View, ScrollView, RefreshControl} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import FocusedStatusBar from '../../components/FocusedStatusBar';
 import DiscussionCard from '../../components/DiscussionCard';
 import SwitchSelector from 'react-native-switch-selector';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {colors, themes, typography, spacing} from '../../styles';
-import {useDiscussion} from '../../contexts/discussionContext';
+
+import {discussionGetList} from '../../api/discussions/discussions.api';
+import {useSecureStorage} from '../../hooks/useSecureStorage';
 
 //switch-selector options
 const options = [
@@ -22,9 +24,22 @@ const options = [
 //function return
 function DiscussionScreen(props) {
   const theme = themes.light;
-  const [discussions] = useDiscussion([]);
-  console.log(discussions);
+  const [token, setToken] = useSecureStorage('userToken', '');
+  const [discussions, setDiscussions] = useState(null);
+  const [filterDiscussion, setFilterDiscussion] = useState(null);
+  const [refreshing, setRefreshing] = useState(false);
+  const [reloadData, setReloadData] = useState(reloadData);
 
+  useEffect(() => {
+    discussionGetList(token)
+      .then((response) => {
+        setDiscussions(response);
+        sortDate(response);
+      })
+      .catch((err) => {});
+  }, [token, reloadData]);
+
+  // function format date Jan 30th, 2021
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     const year = new Intl.DateTimeFormat('en', {year: 'numeric'}).format(date);
@@ -33,6 +48,26 @@ function DiscussionScreen(props) {
 
     return `${month} ${day}, ${year}`;
   };
+
+  // sort the discussion list by updated Date
+  const sortDate = (data) => {
+    const array = data.sort((item1, item2) => {
+      return Date.parse(item2.updatedAt) - Date.parse(item1.updatedAt);
+    });
+    setFilterDiscussion(array); // set Filter discussion
+  };
+
+  // wait time for refresh
+  const wait = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
+
+  //handle on refresh
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true); // enable refresh indicator
+    setReloadData(!reloadData); // change the reloadData to re-render new Discussion
+    wait(1000).then(() => setRefreshing(false)); // hide refresh indicator
+  }, [reloadData]);
 
   return (
     <SafeAreaView style={{flex: 1}} edges={['right', 'left']}>
@@ -56,25 +91,42 @@ function DiscussionScreen(props) {
         buttonColor={colors.primary500}
         options={options}
         initial={0}
-        onPress={(value) => console.log(`${value}`)}
+        onPress={(value) => {
+          switch (value) {
+            case 'Recent':
+              sortDate(discussions);
+          }
+        }}
       />
 
-      <ScrollView horizontal={false}>
-        {discussions
-          ? discussions.map((discussion) => (
-              <DiscussionCard
-                key={discussion._id}
-                title={discussion.title}
-                nameAndDate={`${discussion.owner.firstName} ${
-                  discussion.owner.lastName
-                } Posed ${formatDate(discussion.createdAt)}`}
-                description={discussion.description}
-                categories={discussion.categories}
-                replies={discussion.replies}
-              />
-            ))
-          : null}
-      </ScrollView>
+      <View style={styles.containerBody}>
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              tintColor={colors.primary900}
+            />
+          }
+          horizontal={false}
+          contentInset={{bottom: 150}}
+          contentInsetAdjustmentBehavior="automatic">
+          {filterDiscussion
+            ? filterDiscussion.map((discussion) => (
+                <DiscussionCard
+                  key={discussion._id}
+                  title={discussion.title}
+                  nameAndDate={`${discussion.owner.firstName} ${
+                    discussion.owner.lastName
+                  } Posed ${formatDate(discussion.createdAt)}`}
+                  description={discussion.description}
+                  categories={discussion.categories}
+                  replies={discussion.replies}
+                />
+              ))
+            : null}
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
@@ -93,6 +145,13 @@ const styles = StyleSheet.create({
     textAlignVertical: 'center',
   },
 
+  containerBody: {
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    borderRadius: 50,
+    margin: spacing.small,
+    textAlignVertical: 'center',
+  },
   //create New Discussion style
   createNewButton: {
     height: 25,
