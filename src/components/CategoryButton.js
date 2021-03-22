d1e35a1fbe36ea5fab0702320e1b596eaa6dc60e
duon0060 <duon0060@algonquinlive.com>
2021-03-21 23:28:14 -0400

    Use image icon for category button and styling

--- src/components/CategoryButton.js
+++ src/components/CategoryButton.js
@@ -1,42 +1,56 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { colors, typography, spacing } from '../styles'
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import {colors, typography, spacing} from '../styles';
 
 //card to display category button
 export default function CategoryButton(props) {
-    return (
-        <View>
-            <TouchableOpacity style={styles.categoryButton}>
-                <Text style={styles.categoryIcon}>{props.icon}</Text>
-            </TouchableOpacity>
-            <Text style={styles.categoryName}>{props.name}</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.categoryButton}>
+        <Image
+          style={styles.categoryIcon}
+          source={{
+            uri: props.icon,
+          }}
+        />
+      </TouchableOpacity>
+      <Text style={styles.categoryName}>{props.name}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  //styling for category button
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: 120,
+  },
 
-    //styling for category button
-    categoryButton: {
-        width: 80,
-        height: 80,
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowOffset: { width: 3, height: 6 },
-        shadowColor: colors.gray900,
-        shadowOpacity: 0.2,
-        borderRadius: 100,
-        backgroundColor: colors.primary50,
-    },
-    categoryIcon: {
-        color: colors.primary900,
-        fontSize: 40,
-    },
-    categoryName: {
-        color: colors.primary900,
-        fontWeight: "600",
-        alignSelf: "center",
-        marginTop: spacing.small,
-        fontSize: typography.fs2
-    }
-})
+  categoryButton: {
+    width: 80,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowOffset: {width: 3, height: 6},
+    shadowColor: colors.gray900,
+    shadowOpacity: 0.2,
+    borderRadius: 100,
+    backgroundColor: colors.primary50,
+  },
+  categoryIcon: {
+    color: colors.primary900,
+    height: 40,
+    width: 40,
+    borderRadius: 40 / 2,
+  },
+  categoryName: {
+    color: colors.primary900,
+    fontWeight: '600',
+    alignSelf: 'center',
+    marginTop: spacing.small,
+    fontSize: typography.fs2,
+    textAlign: 'center',
+  },
+});
