a180a34548c7e9dc4002d440be69610a4551873b
duon0060 <duon0060@algonquinlive.com>
2021-03-21 23:28:42 -0400

    Add attachment image functionality

--- src/components/CreateDiscussion.js
+++ src/components/CreateDiscussion.js
@@ -11,13 +11,17 @@ import {
   ScrollView,
   TouchableOpacity,
   Alert,
+  TouchableHighlight,
+  Image,
 } from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import {colors, themes, typography, spacing} from '../styles';
-import {useSecureStorage} from '../hooks/useSecureStorage';
 import {discussionAdd} from '../api/discussions/discussions.api';
 import {useCurrentUser} from '../contexts/currentUserContext';
 import Loader from './Loader';
 import MessageModal from './MessageModal';
+import s3Storage from '../api/aws/s3Strorage';
 
 //function return
 function CreateDiscussion(props) {
@@ -30,6 +34,7 @@ function CreateDiscussion(props) {
   const [discussions, setDiscussions] = useState(null);
   const [loading, setLoading] = useState(false);
   const [showing, setShowing] = useState(false);
+  const [imageUri, setImageUri] = useState('');
 
   // function handle when user tap Post discsussion button
   const handlePostDiscussion = () => {
@@ -61,10 +66,48 @@ function CreateDiscussion(props) {
       });
   };
 
+  //getCameraRoll
+
+  const getPicturesFromGallery = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        maxWidth: 640,
+        maxHeight: 480,
+        quality: 0.8,
+        includeBase64: true,
+      },
+      (response) => {
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        } else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton);
+        } else {
+          console.log(response);
+          setImageUri(response.base64);
+          s3Storage(response.base64);
+        }
+      },
+    );
+  };
+
   return (
     <View style={styles.baseModal}>
       <Loader loading={loading} />
       <MessageModal showing={showing} message="Discussion Posted" />
+      {/* <Image
+        style={{height: 100}}
+        source={
+          1
+            ? {
+                uri:
+                  'https://indigenous-images.s3.amazonaws.com/abbuseIcon.png',
+              }
+            : require('../testImages/demoPic.png')
+        }
+      /> */}
       <ScrollView>
         <View>
           <View style={styles.discussionTopic}>
@@ -85,9 +128,11 @@ function CreateDiscussion(props) {
               value={discussionDescription}
               onChangeText={setDiscussionDescription}
             />
-          <TouchableOpacity style={styles.imageButton}>
-                            <Text style={styles.imageButtonText}>Attach Picture</Text>
-          </TouchableOpacity>
+            <TouchableHighlight
+              style={styles.imageButton}
+              onPress={() => getPicturesFromGallery()}>
+              <Text style={styles.imageButtonText}>Attach Picture</Text>
+            </TouchableHighlight>
           </View>
           <View style={styles.discussionCategory}>
             <Text style={styles.cardTitle}>Category (Optional)</Text>
@@ -96,7 +141,6 @@ function CreateDiscussion(props) {
               placeholder="None Selected"
             />
           </View>
-
         </View>
       </ScrollView>
       <View style={styles.buttonsGroup}>
@@ -144,23 +188,20 @@ const styles = StyleSheet.create({
     paddingHorizontal: spacing.hairline,
   },
 
-
-    //styling for discussion Description container 
-    discussionDescription: {
-        height: '60%',
-        alignItems: 'flex-start',
-        padding: spacing.base,
-        backgroundColor: colors.white,
-        marginBottom: spacing.small,
-    },
-    descriptionInput: {
-        width: '100%',
-        height: 280,
-        paddingHorizontal: spacing.hairline,
-        lineHeight: typography.lh4,
-    },
-
-
+  //styling for discussion Description container
+  discussionDescription: {
+    height: '60%',
+    alignItems: 'flex-start',
+    padding: spacing.base,
+    backgroundColor: colors.white,
+    marginBottom: spacing.small,
+  },
+  descriptionInput: {
+    width: '100%',
+    height: 280,
+    paddingHorizontal: spacing.hairline,
+    lineHeight: typography.lh4,
+  },
 
   //styling for choosing categories container
   discussionCategory: {
@@ -180,44 +221,43 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
 
-    //styling for bottom buttons group
-    buttonsGroup: {
-        flexDirection: 'row',
-        width: "100%",
-        backgroundColor: colors.white,
-        justifyContent: 'space-around',
-        paddingHorizontal: spacing.base,
-        paddingTop: spacing.base
-    },
-    buttonContainer: {
-        width: "40%",
-        borderRadius: 10,
-        marginBottom: spacing.small,
-        backgroundColor: colors.primary500,
-        paddingVertical: spacing.small,
-        paddingHorizontal: spacing.small
-    },
-    buttonText: {
-        alignSelf: "center",
-        fontSize: typography.fs2,
-        color: colors.white,
-        fontWeight: typography.fwBold
-    },
+  //styling for bottom buttons group
+  buttonsGroup: {
+    flexDirection: 'row',
+    width: '100%',
+    backgroundColor: colors.white,
+    justifyContent: 'space-around',
+    paddingHorizontal: spacing.base,
+    paddingTop: spacing.base,
+  },
+  buttonContainer: {
+    width: '40%',
+    borderRadius: 10,
+    marginBottom: spacing.small,
+    backgroundColor: colors.primary500,
+    paddingVertical: spacing.small,
+    paddingHorizontal: spacing.small,
+  },
+  buttonText: {
+    alignSelf: 'center',
+    fontSize: typography.fs2,
+    color: colors.white,
+    fontWeight: typography.fwBold,
+  },
 
-    imageButton: {
-        width: "40%",
-        borderRadius: 10,
-        marginBottom: spacing.small,
-        alignSelf: 'flex-end',
-        backgroundColor: colors.primary50,
-        paddingVertical: spacing.small,
-        paddingHorizontal: spacing.small
-    },
-    imageButtonText: {
-        alignSelf: "center",
-        fontSize: typography.fs2,
-        color: colors.primary900,
-        fontWeight: typography.fwNormal
-    },
-
+  imageButton: {
+    width: '40%',
+    borderRadius: 10,
+    marginBottom: spacing.small,
+    alignSelf: 'flex-end',
+    backgroundColor: colors.primary50,
+    paddingVertical: spacing.small,
+    paddingHorizontal: spacing.small,
+  },
+  imageButtonText: {
+    alignSelf: 'center',
+    fontSize: typography.fs2,
+    color: colors.primary900,
+    fontWeight: typography.fwNormal,
+  },
 });
