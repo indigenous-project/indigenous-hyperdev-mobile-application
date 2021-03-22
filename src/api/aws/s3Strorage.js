14f829ff88de01310597a853c1e9d47500e5c6e8
duon0060 <duon0060@algonquinlive.com>
2021-03-21 23:27:55 -0400

    make api store image in S3

---
+++ src/api/aws/s3Strorage.js
@@ -0,0 +1,31 @@
+// API for store an image in S3
+
+import AWS from 'aws-sdk';
+import Base64Binary from 'base64-arraybuffer';
+import {Alert} from 'react-native';
+const ID = 'AKIAWBK3UNFZVVF7OF4B';
+const SECRET = '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';
+const BUCKET_NAME = 'indigenous-images';
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET,
+});
+
+export default async function s3Storage(fileContent) {
+  const imageBody = await Base64Binary.decode(fileContent);
+  if (imageBody) {
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: Math.floor(Math.random() * 10000000000).toString(),
+      ContentType: 'image/jpeg',
+      Body: imageBody,
+      ACL: 'public-read-write',
+    };
+    s3.upload(params, async function (err, data) {
+      if (err) {
+        Alert.alert('Upload image Failed');
+      }
+      console.log('File uploaded successfully.', data);
+    });
+  }
+}
