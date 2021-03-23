af39b46ec88c5a506975e13bd7d6e21b060ceae8
duon0060 <duon0060@algonquinlive.com>
2021-03-22 23:34:17 -0400

    add right header button

--- src/screens/services/ServicesStack.js
+++ src/screens/services/ServicesStack.js
@@ -7,6 +7,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import ServiceScreen from './ServiceScreen';
 import {themes} from '../../styles';
 import HambugerMenuHeader from '../../components/HambugerMenuHeader';
+import RightHeaderButton from '../../components/RightHeaderButton';
 
 const theme = themes.light;
 const Services = createStackNavigator();
@@ -19,6 +20,7 @@ function ServicesStack({navigation}) {
         headerStyle: {backgroundColor: theme.primaryColor}, //header background
         headerTintColor: theme.inverseTextColor, // text color
         headerLeft: () => <HambugerMenuHeader navigationProps={navigation} />, // implement hambuger menu on the left of the header
+        headerRight: () => <RightHeaderButton navigationProps={navigation} />, // implement right header buttons: search, notification
       }}>
       <Services.Screen name="Services" component={ServiceScreen} />
     </Services.Navigator>
