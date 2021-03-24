cf31f62f406bddd78213ecc5928ceb67cd55f375
duon0060 <duon0060@algonquinlive.com>
2021-03-24 02:12:02 -0400

    pass image uri

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,46 +1,45 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import { colors, typography, spacing } from '../styles'
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {colors, typography, spacing} from '../styles';
 
 //card to display upcoming events
 export default function EventCard(props) {
-    return (
-        <View style={styles.eventCard}>
-            <Image style={styles.image} source={require('../testImages/demoPic.png')} />
-            <Text style={styles.cardTitle}>{props.name}</Text>
-            <Text style={styles.cardSubTitle}>{props.date}</Text>
-            <Text style={styles.cardDetail}>{props.status}</Text>
-        </View>
-    );
+  return (
+    <View style={styles.eventCard}>
+      <Image style={styles.image} source={{uri: props.image}} />
+      <Text style={styles.cardTitle}>{props.name}</Text>
+      <Text style={styles.cardSubTitle}>{props.date}</Text>
+      <Text style={styles.cardDetail}>{props.status}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-
-    //styling for upcoming event
-    eventCard: {
-        margin: spacing.small,
-        backgroundColor: colors.white,
-        shadowOffset: { width: 3, height: 6 },
-        shadowColor: colors.gray900,
-        shadowOpacity: 0.2,
-    },
-    cardTitle: {
-        fontWeight: typography.fwBold,
-        paddingBottom: spacing.smaller
-    },
-    cardSubTitle: {
-        fontWeight: typography.fwMedium,
-        paddingBottom: spacing.smaller
-    },
-    cardDetail: {
-        fontWeight: typography.fwLight,
-        paddingBottom: spacing.smaller
-    },
-    image: {
-        width: 200,
-        height: 100,
-        borderTopLeftRadius: spacing.small,
-        borderTopRightRadius: spacing.small,
-        marginBottom: spacing.small
-    },
-})
+  //styling for upcoming event
+  eventCard: {
+    margin: spacing.small,
+    backgroundColor: colors.white,
+    shadowOffset: {width: 3, height: 6},
+    shadowColor: colors.gray900,
+    shadowOpacity: 0.2,
+  },
+  cardTitle: {
+    fontWeight: typography.fwBold,
+    paddingBottom: spacing.smaller,
+  },
+  cardSubTitle: {
+    fontWeight: typography.fwMedium,
+    paddingBottom: spacing.smaller,
+  },
+  cardDetail: {
+    fontWeight: typography.fwLight,
+    paddingBottom: spacing.smaller,
+  },
+  image: {
+    width: 200,
+    height: 100,
+    borderTopLeftRadius: spacing.small,
+    borderTopRightRadius: spacing.small,
+    marginBottom: spacing.small,
+  },
+});
