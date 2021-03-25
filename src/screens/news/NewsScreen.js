460e3831088cab0e8480362b683be70630b79cb7
abhishekbaazaru <baaz0002@algonquinlive.com>
2021-03-25 04:41:09 -0400

    Updated News Detail Screen

--- src/screens/news/NewsScreen.js
+++ src/screens/news/NewsScreen.js
@@ -3,7 +3,13 @@
 // import packages
 import React from 'react';
 
-import {View, ScrollView, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import FocusedStatusBar from '../../components/FocusedStatusBar';
 import JobCard from '../../components/JobCard';
@@ -12,7 +18,7 @@ import NewsCard from '../../components/NewsCard';
 import {colors, spacing, themes, typography} from '../../styles';
 
 //function return
-function NewsScreen(props) {
+function NewsScreen({navigation}) {
   const theme = themes.light;
 
   return (
@@ -50,10 +56,12 @@ function NewsScreen(props) {
         </View>
 
         <View style={styles.container}>
-          <NewsCard
-            title="North Bay expands its Education Opportunities for"
-            date="Feb 10, 2021"
-            details="North Bay Indigenous Friendship Center provides"></NewsCard>
+          <TouchableOpacity onPress={() => navigation.navigate('News Article')}>
+            <NewsCard
+              title="North Bay expands it's Education Opportunities for"
+              date="Feb 10, 2021"
+              details="North Bay Indigenous Friendship Center provides"></NewsCard>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
