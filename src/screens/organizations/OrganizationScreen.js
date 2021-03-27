edb1f5175bf8438a882c3d46f3dd39ec5ce85cb4
anoj001 <anoj0001@algonquinlive.com>
2021-03-27 04:21:55 -0400

    Implimentation of API in Organization Screen

--- src/screens/organizations/OrganizationScreen.js
+++ src/screens/organizations/OrganizationScreen.js
@@ -2,43 +2,50 @@
 
 // import packages
 import React, {useEffect, useState} from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
+import {Alert, ScrollView, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import FocusedStatusBar from '../../components/FocusedStatusBar';
-import {spacing, themes, typography} from '../../styles';
+import {spacing, themes} from '../../styles';
 import OrganizationChips from '../../components/OrganizationChips';
 import OrganizationListViews from '../../components/OrganizationListViews';
 import MapViews from '../../components/MapViews';
 import {View} from 'native-base';
 import SwitchSelector from 'react-native-switch-selector';
 
-//function return
-function OrganizationScreen(props) {
-  const [stateSelector, setStateSelector] = useState('listView');
-  console.log(stateSelector);
+import {organizationGetList} from '../../api/organizations/organizations.api';
+import {useCurrentUser} from '../../contexts/currentUserContext';
+import {useIsFocused} from '@react-navigation/core';
 
-  const viewOptions = [
-    {
-      label: 'List',
-      value: 'listView',
-    },
-    {label: 'Map', value: 'mapView'},
+//function return
+function OrganizationScreen() {
+  const [stateSelector, setStateSelector] = useState(null);
+  const [currentUser, token] = useCurrentUser();
+  const [reloadData, setReloadData] = useState(reloadData);
+  const isFocused = useIsFocused();
+  const [organizationList, setOrganizationList] = useState();
+
+  //options for switch selectors
+  const options = [
+    {label: 'List', value: 1},
+    {label: 'Map', value: 2},
   ];
 
-  const selectedView = (value) => {
-    switch (value) {
-      case 'listView':
-        // list view of the orgnizations;
-        setStateSelector(value);
-        return value;
+  //useEffect to load organization list
+  useEffect(() => {
+    organizationGetList(token)
+      .then((response) => {
+        stateSelector === null ? setStateSelector(1) : null; // set initial stateSelector = listView
+        if (response) {
+          //  checking state of Selector
+          setOrganizationList(response);
+        }
+      })
+      .catch((err) => {
+        Alert.alert(err.errors[0].title, err.errors[0].description);
+      });
+  }, [token, reloadData, stateSelector, isFocused]);
 
-      case 'mapView':
-        // map view of the orgnizations;
-        setStateSelector(value);
-        return value;
-    }
-  };
-
+  //dynamic data
   const data = {
     name: 'North Bay Medical Care',
     rating: '5.0',
@@ -48,6 +55,8 @@ function OrganizationScreen(props) {
     image:
       'https://images.unsplash.com/photo-1615484486786-5a3732131c13?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2251&q=80',
   };
+  if (!organizationList) return null;
+
   return (
     <SafeAreaView style={styles.safeArea} edges={['right', 'left']}>
       <FocusedStatusBar barStyle="light-content" />
@@ -64,25 +73,36 @@ function OrganizationScreen(props) {
       {/* Custom Switch Selectors */}
       <View style={styles.switchView}>
         <SwitchSelector
-          options={viewOptions}
+          options={options}
           initial={0}
           textColor={themes.light.primaryColor}
           bold={true}
           borderRadius={10}
-          animationDuration={200}
           height={27}
           selectedColor={themes.light.inverseTextColor}
           buttonColor={themes.light.primaryColor}
-          onPress={selectedView}
+          onPress={(value) => {
+            switch (value) {
+              case 1:
+                // list view of the orgnizations;
+                setStateSelector(1);
+                break;
+
+              case 2:
+                // map view of the orgnizations;
+                setStateSelector(2);
+                break;
+            }
+          }}
         />
       </View>
-
-      {stateSelector == 'listView' ? (
+      {stateSelector == 1 ? (
         // List View
-        <OrganizationListViews listofData={data} />
-      ) : stateSelector == 'mapView' ? (
-        //  Map View 
+        <OrganizationListViews organizationList={organizationList} />
+      ) : stateSelector == 2 ? (
+        //  Map View
         <MapViews
+          // organizationList={organizationList}
           latitude={45.35611}
           longitude={-75.757248}
           description="My Location"
