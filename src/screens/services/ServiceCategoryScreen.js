a71fc9a8a92356189f8d24ec4fda4e39afc614e6
duon0060 <duon0060@algonquinlive.com>
2021-03-30 23:05:16 -0400

    handle last open service and store it in storage

--- src/screens/services/ServiceCategoryScreen.js
+++ src/screens/services/ServiceCategoryScreen.js
@@ -1,5 +1,5 @@
-import { SecretsManager, ServiceCatalog } from 'aws-sdk';
-import React, { useEffect, useState } from 'react';
+import {SecretsManager, ServiceCatalog} from 'aws-sdk';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -8,19 +8,23 @@ import {
   TouchableOpacity,
   Modal,
   Pressable,
+  Alert,
 } from 'react-native';
-import { serviceGetList } from '../../api/services/services.api';
-import { spacing, colors, typography } from '../../styles';
+import {serviceGetList} from '../../api/services/services.api';
+import {spacing, colors, typography} from '../../styles';
 import ServicesCard from '../../components/ServicesCard';
 import ServiceDetail from '../../components/ServiceDetail';
+import {useAsyncStorage} from '../../hooks/useAsyncStorage';
 
-const ServiceCategoryScreen = ({ navigate, route }) => {
+const ServiceCategoryScreen = ({navigate, route}) => {
   // console.log(props)
   const token = route.params.token;
-  const [selectedService, setSelctedService] = useState(null);
+  const [selectedService, setSelectedService] = useState(null);
+
   const [modalVisible, setModalVisible] = useState(false);
   const serviceId = route.params.name;
   const [filterServices, setFilteredServices] = useState(null);
+  const [storeLastOpen, setStoreLastOpen] = useAsyncStorage('lastOpen', null);
 
   const filterServiceByCategory = (data) => {
     const array = data.filter((item) => {
@@ -43,28 +47,39 @@ const ServiceCategoryScreen = ({ navigate, route }) => {
       });
   }, [token]);
 
+  const handleLastOpen = (id) => {
+    if (storeLastOpen[0] !== id && storeLastOpen[1] !== id) {
+      storeLastOpen.unshift(id);
+      storeLastOpen.length > 2 ? storeLastOpen.pop() : null;
+      setStoreLastOpen(storeLastOpen);
+    }
+  };
+
   if (!filterServices) return null;
   // console.log(filterServices)
+  //console.log('hehehesadasdsadsda');
   return (
-    <SafeAreaView style={{ flex: 1 }} edges={['right', 'left']}>
+    <SafeAreaView style={{flex: 1}} edges={['right', 'left']}>
       <View style={styles.container}>
         <Text style={styles.heading}>{serviceId}</Text>
 
-        {filterServices
+        {filterServices.length > 0
           ? filterServices.map((service) => (
-            <TouchableOpacity
-              onPress={() =>
-                setSelctedService(service._id) & setModalVisible(true)
-              }
-              key={service._id}>
-              <ServicesCard
-                key={service._id}
-                title={service.name}
-                name={service.contact.providerName}
-                description={service.contact.position}
-              />
-            </TouchableOpacity>
-          ))
+              <TouchableOpacity
+                onPress={() => {
+                  setSelectedService(service._id);
+                  setModalVisible(true);
+                  handleLastOpen(service);
+                }}
+                key={service._id}>
+                <ServicesCard
+                  key={service._id}
+                  title={service.name}
+                  name={service.contact.providerName}
+                  description={service.contact.position}
+                />
+              </TouchableOpacity>
+            ))
           : null}
       </View>
 
@@ -76,34 +91,36 @@ const ServiceCategoryScreen = ({ navigate, route }) => {
           Alert.alert('Modal has been closed.');
           setModalVisible(!modalVisible);
         }}>
-        {filterServices
-          .filter(function (service) {
-            return service._id === selectedService;
-          })
-          .map((service) => (
-            <View style={styles.modalView} key={service._id}>
-              <View style={styles.modalTitle}>
-                <View>
-                  <Text style={styles.modalTitleText}>{service.name}</Text>
-                </View>
+        {filterServices.length > 0
+          ? filterServices
+              .filter(function (service) {
+                return service._id === selectedService;
+              })
+              .map((service) => (
+                <View style={styles.modalView} key={service._id}>
+                  <View style={styles.modalTitle}>
+                    <View>
+                      <Text style={styles.modalTitleText}>{service.name}</Text>
+                    </View>
 
-                <Pressable
-                  style={styles.closeButton}
-                  onPress={() => setModalVisible(!modalVisible)}
-                  key={service._id}>
-                  <Text style={styles.buttonText}>x</Text>
-                </Pressable>
-              </View>
-              <ServiceDetail
-                serviceProviderName={service.contact.providerName}
-                serviceProviderPosition={service.contact.position}
-                contactEmail={service.contact.email}
-                contactPhone={service.contact.phone}
-                description={service.description}
-                media={service.media}
-              />
-            </View>
-          ))}
+                    <Pressable
+                      style={styles.closeButton}
+                      onPress={() => setModalVisible(!modalVisible)}
+                      key={service._id}>
+                      <Text style={styles.buttonText}>x</Text>
+                    </Pressable>
+                  </View>
+                  <ServiceDetail
+                    serviceProviderName={service.contact.providerName}
+                    serviceProviderPosition={service.contact.position}
+                    contactEmail={service.contact.email}
+                    contactPhone={service.contact.phone}
+                    description={service.description}
+                    media={service.media}
+                  />
+                </View>
+              ))
+          : null}
       </Modal>
     </SafeAreaView>
   );
@@ -154,7 +171,7 @@ const styles = StyleSheet.create({
     width: 25,
     height: 25,
     alignItems: 'center',
-    shadowOffset: { width: 3, height: 3 },
+    shadowOffset: {width: 3, height: 3},
     shadowColor: colors.gray900,
     shadowOpacity: 0.2,
     borderRadius: 100,
