1cd2565f4dd9faa30a24510c0933906d6e46006c
Minal Patel <pate0773@algonquinlive.com>
2021-03-31 11:56:34 -0400

    basic layout for AboutUs and IndigenousPeople Screen

--- src/screens/sidenavbar/IndigenousPeopleScreen.js
+++ src/screens/sidenavbar/IndigenousPeopleScreen.js
@@ -3,18 +3,58 @@
 // import packages
 import React from 'react';
 
-import {Text} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import FocusedStatusBar from '../../components/FocusedStatusBar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  View,
+  ScrollView,
+  StyleSheet,
+  Text,
+  Image,
+} from 'react-native';
+
+import { colors, themes, typography, spacing } from '../../styles';
+import BackButtonHeaderLeft from '../../components/BackButtonHeaderLeft';
 
 //function return
-function IndigenousPeopleScreen(props) {
+function IndigenousPeopleScreen({ navigation }) {
+  const theme = themes.light;
+
   return (
-    <SafeAreaView style={{flex: 1}} edges={['right', 'bottom', 'left']}>
-      <FocusedStatusBar barStyle="light-content" />
-      <Text>IndigenousPeopleScreen</Text>
+    <SafeAreaView
+      style={styles.safeArea}
+      edges={['right', 'top', 'left', 'bottom']}>
+      <View style={styles.headerContainer}>
+        <BackButtonHeaderLeft navigationProps={navigation} />
+        <Text style={styles.heading}>Indigenous Peoples</Text>
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  headerContainer: {
+    flexDirection: 'row', width: '100%',
+    shadowColor: colors.gray900,
+    height: 45,
+    marginTop: spacing.base,
+    shadowOpacity: 0.2,
+    backgroundColor: colors.white,
+    shadowOffset: { width: 3, height: 6 },
+  },
+  heading: {
+    color: colors.primary900,
+    fontWeight: typography.fwBold,
+    fontSize: typography.fs3,
+    justifyContent: 'center',
+    textAlign: 'center',
+    paddingRight: spacing.larger,
+    paddingTop: spacing.smallest,
+    width: '90%'
+  },
+});
 
 export default IndigenousPeopleScreen;
