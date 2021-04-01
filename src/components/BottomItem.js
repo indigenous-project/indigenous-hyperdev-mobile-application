412475db20000303e828036b936269c3cd363c64
duon0060 <duon0060@algonquinlive.com>
2021-03-31 21:15:15 -0400

    refine the silder bottom navigation

--- src/components/BottomItem.js
+++ src/components/BottomItem.js
@@ -2,23 +2,18 @@ import React, {useState} from 'react';
 import {StyleSheet, Animated, Dimensions, View} from 'react-native';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useEffect} from 'react/cjs/react.development';
 import {colors, themes, typography} from '../styles';
 export const BottomItem = ({iconName, isCurrent, size, color, index}) => {
   const [translateValue] = useState(new Animated.Value(0));
   const totalWidth = Dimensions.get('window').width;
   const tabWidth = totalWidth / 5;
 
-  Animated.spring(translateValue, {
-    toValue: index * tabWidth,
-    velocity: 10,
-    useNativeDriver: true,
-  }).start();
-
   return (
     <>
       <Animated.View
         style={[
-          styles.slider,
+          isCurrent ? styles.slider : styles.sliderDisable,
           {
             transform: [{translateX: translateValue}],
             width: tabWidth - 20,
@@ -35,6 +30,15 @@ const styles = StyleSheet.create({
   icon: {
     marginVertical: 0,
     fontSize: typography.fs6,
+  },
+  sliderDisable: {
+    height: 4,
+    position: 'absolute',
+    top: 0,
+    left: 10,
+
+    borderRadius: 10,
+    width: 50,
   },
 
   slider: {
