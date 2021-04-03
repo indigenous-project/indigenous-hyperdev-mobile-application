ac2a7d7d10573b3394b36345369093d18389356e
duon0060 <duon0060@algonquinlive.com>
2021-04-02 22:49:07 -0400

    make list of centers context

---
+++ src/contexts/organizationContext.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+
+import {Alert} from 'react-native';
+import {organizationGetList} from '../api/organizations/organizations.api';
+
+import {useSecureStorage} from '../hooks/useSecureStorage';
+
+const OrganizationContext = React.createContext();
+
+function OrganizationProvider(props) {
+  const [organization, setOrganizations] = useState(null);
+  const [token, setToken] = useSecureStorage('userToken', '');
+
+  useEffect(() => {
+    if (token)
+      organizationGetList(token)
+        .then(setOrganizations)
+        .catch((err) => Alert.alert(err.errors[0]));
+  }, [token, props.isFocused, setOrganizations]);
+  return (
+    <OrganizationContext.Provider
+      value={[organization, setOrganizations]}
+      {...props}
+    />
+  );
+}
+
+function useOrganization() {
+  const context = React.useContext(OrganizationContext);
+  if (!context)
+    throw new Error(
+      'useOrganization hook must be called within a OrganizationContext',
+    );
+  return context;
+}
+
+export {OrganizationProvider, useOrganization};
