e6a850120c1e5d74882095228991523551e3e985
duon0060 <duon0060@algonquinlive.com>
2021-04-09 02:38:35 -0400

    change use provider

--- src/screens/sidenavbar/DrawerRoute.js
+++ src/screens/sidenavbar/DrawerRoute.js
@@ -14,6 +14,13 @@ import {CurrentUserProvider} from '../../contexts/currentUserContext';
 import DisclaimerScreen from './DisclaimerScreen';
 import DisclaimerReviewScreen from './DisclaimerReviewScreen';
 import {CategoriesGeneralProvider} from '../../contexts/categoriesGeneralContext';
+
+import {DiscussionProvider} from '../../contexts/discussionContext';
+import {EventProvider} from '../../contexts/eventContext';
+import {JobProvider} from '../../contexts/jobContext';
+import {NewsProvider} from '../../contexts/newsContext';
+import {ServiceProvider} from '../../contexts/serviceContext';
+import {OrganizationProvider} from '../../contexts/organizationContext';
 //////////////////////////////////////////////////////////////////////////////////
 
 //Define a drawer navigator
@@ -24,43 +31,70 @@ const DrawerRoute = () => {
   //Render elements
   return (
     /*Use current user provider */
+
     <CurrentUserProvider>
-      {/*Use category provider */}
-      <CategoriesGeneralProvider>
-        <Drawer.Navigator
-          drawerContent={(props) => <SideNavCustomContent {...props} />} // Use custom Side nav bar
-          drawerPosition="left"
-          backBehavior="history"
-          drawerType="front"
-          hideStatusBar={true}
-          initialRouteName="BottomTabScreen">
-          {/*BottomTabScreen component */}
-          <Drawer.Screen
-            options={{drawerLabel: 'Home'}}
-            name="BottomTabScreen"
-            component={BottomTabScreen}
-          />
-          {/*About Screen component */}
-          <Drawer.Screen name="AboutScreen" component={AboutScreen} />
-          {/*Indigenous People Screen component */}
-          <Drawer.Screen
-            name="IndigenousPeopleScreen"
-            component={IndigenousPeopleScreen}
-          />
-          {/*Ask question Screen component */}
-          <Drawer.Screen
-            name="AskQuestionScreen"
-            component={AskQuestionScreen}
-          />
-          {/*DisclaimerScreen component */}
-          <Drawer.Screen name="DisclaimerScreen" component={DisclaimerScreen} />
-          {/*DisclaimerReviewScreen component */}
-          <Drawer.Screen
-            name="DisclaimerReviewScreen"
-            component={DisclaimerReviewScreen}
-          />
-        </Drawer.Navigator>
-      </CategoriesGeneralProvider>
+      {/* Apply Discussion provider */}
+      <DiscussionProvider>
+        {/* Apply Event provider */}
+        <EventProvider>
+          {/* Apply Job provider */}
+          <JobProvider>
+            {/* Apply News provider */}
+            <NewsProvider>
+              {/* Apply Service provider */}
+              <ServiceProvider>
+                {/* Apply Organization provider */}
+                <OrganizationProvider>
+                  {/*Use category provider */}
+                  <CategoriesGeneralProvider>
+                    <Drawer.Navigator
+                      drawerContent={(props) => (
+                        <SideNavCustomContent {...props} />
+                      )} // Use custom Side nav bar
+                      drawerPosition="left"
+                      backBehavior="history"
+                      drawerType="front"
+                      hideStatusBar={true}
+                      initialRouteName="BottomTabScreen">
+                      {/*BottomTabScreen component */}
+                      <Drawer.Screen
+                        options={{drawerLabel: 'Home'}}
+                        name="BottomTabScreen"
+                        component={BottomTabScreen}
+                      />
+                      {/*About Screen component */}
+                      <Drawer.Screen
+                        name="AboutScreen"
+                        component={AboutScreen}
+                      />
+                      {/*Indigenous People Screen component */}
+                      <Drawer.Screen
+                        name="IndigenousPeopleScreen"
+                        component={IndigenousPeopleScreen}
+                      />
+                      {/*Ask question Screen component */}
+                      <Drawer.Screen
+                        name="AskQuestionScreen"
+                        component={AskQuestionScreen}
+                      />
+                      {/*DisclaimerScreen component */}
+                      <Drawer.Screen
+                        name="DisclaimerScreen"
+                        component={DisclaimerScreen}
+                      />
+                      {/*DisclaimerReviewScreen component */}
+                      <Drawer.Screen
+                        name="DisclaimerReviewScreen"
+                        component={DisclaimerReviewScreen}
+                      />
+                    </Drawer.Navigator>
+                  </CategoriesGeneralProvider>
+                </OrganizationProvider>
+              </ServiceProvider>
+            </NewsProvider>
+          </JobProvider>
+        </EventProvider>
+      </DiscussionProvider>
     </CurrentUserProvider>
   );
 };
